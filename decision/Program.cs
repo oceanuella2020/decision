c0c680ac4c5811c5e42be58504d1795eae1097fe
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-16 23:55:34 +0100

    Add project files.

---
+++ decision/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace decision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a number: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            if (number > 0) Console.WriteLine("Number is positive");
+            else if (number == 0) Console.WriteLine("Number is zero");
+            else Console.WriteLine("Number is negative");
+
+            Console.ReadKey();
+
+        }
+    }
+}
